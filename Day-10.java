5e533a18b3de35e62c066ecb3f3fda06fc3d339b
amanagrawal318 <amanagrawal318@gmail.com>
2020-07-07 19:39:46 +0530

    .

---
+++ Day-10.java
@@ -0,0 +1,33 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Solution {
+
+
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        int n = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        scanner.close();
+        int sum=0,ans=0;
+        while(n>0){
+            if(n%2==1){
+                sum++;
+                ans=Math.max(ans,sum);
+            }else{
+                sum=0;
+            }
+            n=n/2;
+        }
+        System.out.println(ans);
+    }
+}
+
