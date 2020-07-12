ddfdf619645bad6a14c2f80ed736db705552ff36
amanagrawal318 <59931227+amanagrawal318@users.noreply.github.com>
2020-07-12 16:51:09 +0530

    More Exceptions

--- Day-17.java
+++ Day-17.java
@@ -3,7 +3,7 @@
 class Calculator{
      int power(int n,int p) throws Exception{
              if(n<0 || p<0){
-                 throw  new Exception("n and p should be non-negative");
+                throw  new Exception("n and p should be non-negative");
              }
              return (int)Math.pow(n,p);
          }
