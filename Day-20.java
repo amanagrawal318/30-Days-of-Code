85c3dc724f7c11962890a48a4df1a339e1817ee3
amanagrawal318 <59931227+amanagrawal318@users.noreply.github.com>
2020-07-12 16:53:52 +0530

    Sorting

--- Day-20.java
+++ Day-20.java
@@ -16,9 +16,6 @@ public class Solution {
         // Write Your Code Here
         int count = 0;
         for (int i = 0; i < n; i++) {
-    
-            
-    
            for (int j = 0; j < n - 1; j++) {
         
              if (a[j] > a[j + 1]) {
