f1c64852423af53dc484b9499dd755a34005fe63
amanagrawal318 <59931227+amanagrawal318@users.noreply.github.com>
2020-07-12 16:46:49 +0530

    Recursion 3

--- Day-9.java
+++ Day-9.java
@@ -1,7 +1,7 @@
  
     // Complete the factorial function below.
 
-static int factorial(int n) {
+static int factorial(int n){
         if(n==1)
         return 1;
         int myfact=n*factorial(n-1);
