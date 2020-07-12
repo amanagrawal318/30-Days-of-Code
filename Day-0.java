a0a221b5fb121bf937ac0aedc0b5c94b92c55fd6
amanagrawal318 <59931227+amanagrawal318@users.noreply.github.com>
2020-07-12 16:37:40 +0530

    Hello World

--- Day-0.java
+++ Day-0.java
@@ -7,7 +7,7 @@ public class Solution {
 	public static void main(String[] args) {
         
 		Scanner scan = new Scanner(System.in); 
-		
+	
 		
 		String inputString = scan.nextLine(); 
 		scan.close(); 
